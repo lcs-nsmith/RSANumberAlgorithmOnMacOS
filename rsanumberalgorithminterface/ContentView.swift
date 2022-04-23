7ab9178256f65c49833c0bd3dc2fda6127ae3f15
Nathan Smith <nsmith@lcs.on.ca>
2022-04-23 10:31:05 -0400

    finished static layout

--- rsanumberalgorithminterface/ContentView.swift
+++ rsanumberalgorithminterface/ContentView.swift
@@ -8,9 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: Stored Properties
+    @State var providedInput = ""
+    
+    // MARK: Computed Properties
+    var input: Double? {
+        guard let input =
+                Double(providedInput),
+              input > 0
+        else {
+            // When the tested are failed, we do not have a valid radius
+            return nil
+        }
+        return input
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            TextField("Radius",
+                      text: $providedInput,
+                      prompt: Text("input here"))
+            // Ternary conditional operator
+            // "One line if statement"
+            //                     CONDITION        TRUE          FALSE
+            .foregroundColor(input == nil ? Color.red : Color.primary)
+            
+            Text(String(input ?? 0))
+        }
     }
 }
 
