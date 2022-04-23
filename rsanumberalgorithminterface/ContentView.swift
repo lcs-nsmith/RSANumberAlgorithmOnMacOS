7cf6fb1db4a0d0ce7f6b8d571e283f97dc4864d3
Nathan Smith <nsmith@lcs.on.ca>
2022-04-23 10:52:08 -0400

    Works, only problem is that it does not update in real time and just does the loading thing until the algorithm has sound the solution

--- rsanumberalgorithminterface/ContentView.swift
+++ rsanumberalgorithminterface/ContentView.swift
@@ -11,11 +11,13 @@ struct ContentView: View {
     
     // MARK: Stored Properties
     @State var providedInput = ""
+    let lowerBound = 2
+    @State var howManyRSANumbers = 0
     
     // MARK: Computed Properties
-    var input: Double? {
+    var input: Int? {
         guard let input =
-                Double(providedInput),
+                Int(providedInput),
               input > 0
         else {
             // When the tested are failed, we do not have a valid radius
@@ -26,16 +28,46 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            TextField("Radius",
-                      text: $providedInput,
-                      prompt: Text("input here"))
-            // Ternary conditional operator
-            // "One line if statement"
-            //                     CONDITION        TRUE          FALSE
-            .foregroundColor(input == nil ? Color.red : Color.primary)
-            
-            Text(String(input ?? 0))
+            HStack {
+                TextField("Radius",
+                          text: $providedInput,
+                          prompt: Text("input here"))
+                // Ternary conditional operator
+                // "One line if statement"
+                //                     CONDITION        TRUE          FALSE
+                .foregroundColor(input == nil ? Color.red : Color.primary)
+                
+                
+                Button(action: {
+                    runAlgorithm()
+                }, label: {
+                    Text("Run Algorithm")
+                })
+                .buttonStyle(.bordered)
+            }
+            HStack {
+                Text("Input:")
+                Text(String(input ?? 0))
+            }
+            HStack {
+                Text("RSA Number Count:")
+                Text(String(howManyRSANumbers))
+            }
         }
+    }
+    func runAlgorithm() {
+        for n in min(lowerBound,2)...(input!) {
+            var divisors = 1
+            for j in 1...n/2 {
+                if n.isMultiple(of: j) {
+                    divisors += 1
+                }
+            }
+            if divisors == 4 {
+                howManyRSANumbers += 1
+            }
+        }
+
     }
 }
 
