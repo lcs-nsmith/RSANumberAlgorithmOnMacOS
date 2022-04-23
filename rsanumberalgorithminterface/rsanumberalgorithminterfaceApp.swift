7f5cbc3c17c613c77508d5eca4627249d384f924
Nathan Smith <nsmith@lcs.on.ca>
2022-04-23 10:06:19 -0400

    Initial Commit

---
+++ rsanumberalgorithminterface/rsanumberalgorithminterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  rsanumberalgorithminterfaceApp.swift
+//  rsanumberalgorithminterface
+//
+//  Created by Nathan Smith on 2022-04-23.
+//
+
+import SwiftUI
+
+@main
+struct rsanumberalgorithminterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
